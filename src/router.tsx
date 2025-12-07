744181f15a0fb0def2c377b43abf87b0f0916395
Mauricio Gonzalez Marmolejo <sandre9477@gmail.com>
2025-12-07 09:19:23 -0600

    work with the layout of admin-user to show the principal information

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,9 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import LoginView from "./Views/LoginView";
 import RegisterView from "./Views/RegisterView";
 import AuthLayout from "./layouts/AuthLayout";
+import AppLayout from "./layouts/AppLayout";
+import LinkTreeView from "./Views/LinkTreeView";
+import ProfileView from "./Views/ProfileView";
 
 export default function Router() {
   return (
@@ -10,6 +13,10 @@ export default function Router() {
         <Route element={<AuthLayout />}>
           <Route path="/auth/login" element={<LoginView />} />
           <Route path="/auth/register" element={<RegisterView />} />
+        </Route>
+        <Route path="/admin" element={<AppLayout />}>
+          <Route index={true} element={<LinkTreeView />} />
+          <Route path="profile" element={<ProfileView />} />
         </Route>
       </Routes>
     </BrowserRouter>
