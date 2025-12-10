03384ed850d9814fd9990412d7ba890cb0ebc843
Sandre-GM <sandre9477@gmail.com>
2025-12-10 09:12:28 -0600

    adding interceptors with axios to protect the admin layout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,17 +1,21 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
 import { useQuery } from "@tanstack/react-query";
 import NavigationTabs from "../components/NavigationTabs";
 import { getUser } from "../api/DevTreeApi";
 
 export default function AppLayout() {
-  const { data, isLoading, error, isError } = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryFn: getUser,
     queryKey: ["user"],
     retry: 1,
     refetchOnWindowFocus: false,
   });
-  console.log({ data, isLoading, error: error?.message, isError });
+  console.log({ data, isLoading, isError });
+  if (isLoading) return "Cargando...";
+  if (isError) {
+    return <Navigate to={"/auth/login"} />;
+  }
   return (
     <>
       <header className="bg-slate-800 py-5">
