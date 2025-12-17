fcf016bbbdb980e98c4f712dbc78292e599766e9
Sandre-GM <sandre9477@gmail.com>
2025-12-16 18:08:42 -0600

    adding a new render-component for the profileview

--- src/Views/ProfileView.tsx
+++ src/Views/ProfileView.tsx
@@ -1,6 +1,27 @@
+import { useForm } from "react-hook-form";
+import ErrorsMessages from "../components/ErrorsMessages";
+
 export default function ProfileView() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      handle: "",
+      description: "",
+    },
+  });
+
+  const handleProfileForm = () => {
+    console.log("Desde handelProfileForm");
+  };
+
   return (
-    <form className="bg-white p-10 rounded-lg space-y-5" onSubmit={() => {}}>
+    <form
+      className="bg-white p-10 rounded-lg space-y-5"
+      onSubmit={handleSubmit(handleProfileForm)}
+    >
       <legend className="text-2xl text-slate-800 text-center">
         Editar Información
       </legend>
@@ -10,7 +31,13 @@ export default function ProfileView() {
           type="text"
           className="border-none bg-slate-100 rounded-lg p-2"
           placeholder="handle o Nombre de Usuario"
+          {...register("handle", {
+            required: "El nombre de usuario es obligatorio",
+          })}
         />
+        {errors.handle && (
+          <ErrorsMessages>{errors.handle.message}</ErrorsMessages>
+        )}
       </div>
 
       <div className="grid grid-cols-1 gap-2">
@@ -18,7 +45,11 @@ export default function ProfileView() {
         <textarea
           className="border-none bg-slate-100 rounded-lg p-2"
           placeholder="Tu Descripción"
+          {...register("description", {
+            required: "La descripción es obligatoria",
+          })}
         />
+        {errors.description && <ErrorsMessages>{errors.description.message}</ErrorsMessages>}
       </div>
 
       <div className="grid grid-cols-1 gap-2">
