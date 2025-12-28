5ce301492c1bc0ac91e89cc9a2f8fb9f5d3e3705
Sandre-GM <sandre9477@gmail.com>
2025-12-28 14:03:38 -0600

    showing the links configuration to LinksView, adding headless ui component too

--- src/Views/LinkTreeView.tsx
+++ src/Views/LinkTreeView.tsx
@@ -1,6 +1,46 @@
+import { useState } from "react";
+import { social } from "../data/social";
+import DevTreeInput from "../components/DevTreeInput";
+import { isValidUrl } from "../utils";
+import { toast } from "sonner";
 
 export default function LinkTreeView() {
+  const [devTreeLinks, setDevTreeLinks] = useState(social);
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLinks = devTreeLinks.map((link) =>
+      link.name === e.target.name ? { ...link, url: e.target.value } : link
+    );
+
+    setDevTreeLinks(updatedLinks);
+  };
+
+  const handleEneableLink = (socialNetworkName: string) => {
+    const updatedLinks = devTreeLinks.map((link) => {
+      if (link.name === socialNetworkName) {
+        if (isValidUrl(link.url)) {
+          return { ...link, enabled: !link.enabled };
+        } else {
+          toast.error(
+            "Por favor ingresa una URL válida antes de habilitar el enlace."
+          );
+        }
+      }
+      return link;
+    });
+    setDevTreeLinks(updatedLinks);
+  };
+
   return (
-    <div>LinkTreeView</div>
-  )
+    <div className="space-y-5">
+      {devTreeLinks.map((item) => (
+        <DevTreeInput
+          key={item.name}
+          item={item}
+          handleUrlChange={handleUrlChange}
+          handleEneableLink={handleEneableLink}
+        />
+      ))}
+    </div>
+  );
 }
